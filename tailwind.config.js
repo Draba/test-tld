8799b8415a56be278db9a2cf48df217a2940037f
Oleksii Kaidalov <finchxx@gmail.com>
2024-03-27 19:12:27 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    backgroundSize: {
+      '20px': '20px',
+    },
+  },
+  plugins: [],
+}
+
